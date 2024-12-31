7b27ece3e9910949aa29f252b8199c154e6266f9
kqito <29191111+kqito@users.noreply.github.com>
2024-12-31 21:28:58 +0900

    make input fn util

--- src/args.rs
+++ src/args.rs
@@ -1,3 +1,6 @@
+use std::io::{self, Read};
+
+use anyhow::Result;
 use argh::FromArgs;
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -20,4 +23,23 @@ pub struct Args {
     /// no-validate
     /// Skip validation of the file
     pub no_validate: Option<bool>,
+}
+
+pub fn get_input() -> Result<Option<String>> {
+    let mut pipeline_input = String::new();
+    let is_piped = atty::isnt(atty::Stream::Stdin);
+
+    if is_piped {
+        if io::stdin().read_to_string(&mut pipeline_input).is_err() {
+            return Err(anyhow::anyhow!("Failed to read from stdin"));
+        }
+    }
+
+    let content = if pipeline_input == "" {
+        Ok(None)
+    } else {
+        Ok(Some(pipeline_input))
+    };
+
+    content
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ mod args;
 mod grep;
 mod output;
 
-use std::io::{self, Read};
-
-use args::Args;
+use args::{get_input, Args};
 use grep::grep;
 use grep::params::GrepParamsBuilder;
 use output::{pretty_print, pretty_println, print_help, Status};
@@ -12,26 +10,18 @@ use output::{pretty_print, pretty_println, print_help, Status};
 fn main() {
     let args: Args = argh::from_env();
 
-    let mut pipeline_input = String::new();
-    let is_piped = atty::isnt(atty::Stream::Stdin);
-
-    if is_piped {
-        if io::stdin().read_to_string(&mut pipeline_input).is_err() {
-            pretty_print("Failed to read from stdin", Status::Error);
+    let input = match get_input() {
+        Ok(input) => input,
+        Err(e) => {
+            pretty_println(&e.to_string(), Status::Error);
             std::process::exit(1);
         }
-    }
-
-    let content = if pipeline_input == "" {
-        None
-    } else {
-        Some(pipeline_input)
     };
 
     let mut params_builder = GrepParamsBuilder::new()
         .no_validate(args.no_validate)
         .unique(args.unique)
-        .content(content);
+        .content(input);
 
     if let Some(f) = args.file {
         match params_builder.read_file_content(&f) {
@@ -47,6 +37,7 @@ fn main() {
         Ok(params) => params,
         Err(e) => {
             // If the program is piped, we don't want to print the error message
+            let is_piped = atty::isnt(atty::Stream::Stdin);
             if is_piped {
                 std::process::exit(0);
             }
